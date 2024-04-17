e017fc3155e607dd70069e147d909a7812c2f611
Minhchau0108 <chaua@coderpush.com>
2024-04-17 08:23:56 +0700

    feat: multiple 300 request send sqs message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,23 +4,30 @@ class MessagesController < ApplicationController
     # Initialize the SQS client
     sqs = Aws::SQS::Client.new(
         region: 'us-east-1',
-        access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-        secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key)
+        access_key_id: 'your-access-key',
+        secret_access_key: 'your-secret-key'
     )
     
 
     # Specify the URL of the SQS queue
     queue_url = 'https://sqs.us-east-1.amazonaws.com/064371329171/test-stripe-queue'
 
-    # Create a message to send to the queue
-    message_body = { name: params[:message_body] }
+    
+    # Create n messages to send to the queue
+    messages = 300.times.map do |i|
+      {
+        id: i.to_s,
+        message_body: { name: "#{params[:message_body]}_#{i}" }.to_json
+      }
+    end
 
-    # Convert the hash to a JSON string
-    message_body_json = message_body.to_json
 
-    puts "Sending message: #{message_body_json} to queue: #{queue_url}"
-
-    sqs.send_message(queue_url: queue_url, message_body: message_body_json)
+    puts "Sending messages to queue: #{queue_url}"
+  
+    # Send the messages in batches of up to 10
+    messages.each_slice(5) do |message_batch|
+      sqs.send_message_batch(queue_url: queue_url, entries: message_batch)
+    end
 
     # Redirect back to the root path after enqueuing the message
     redirect_to root_path, notice: 'Message enqueued successfully!'
